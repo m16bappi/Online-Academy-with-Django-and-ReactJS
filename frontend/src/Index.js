2902ef25cf631b695559adb539cfcd8559628693
Mehedi Hasan <mehedibubt34@gmail.com>
2021-01-12 19:06:57 +0600

    remove old design

--- frontend/src/Index.js
+++ frontend/src/Index.js
@@ -1,14 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Blogs from './components/Blogs/Blogs';
-import BlogDetails from './components/Blogs/BlogDetails/BlogDetails'
+import { HashRouter as Router} from 'react-router-dom';
+
+
 import { Provider } from 'react-redux';
 import store from '../store';
 import { USER_LOADING } from '../store/Actions/Auth/Login'
@@ -22,12 +16,7 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <Fragment>
-            <Header />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/blogs" component={Blogs} />
-              <Route path="/blogs/:id" component={BlogDetails} />
-            </Switch>
+
           </Fragment>
         </Router>
       </Provider>
