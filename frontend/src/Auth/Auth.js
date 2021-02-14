42c0e4a83b3d024e9591ef96f62c7563203b748b
Mehedi Hasan <mehedibubt34@gmail.com>
2021-02-14 20:32:14 +0600

    login update

--- frontend/src/Auth/Auth.js
+++ frontend/src/Auth/Auth.js
@@ -4,16 +4,15 @@ import {
     Avatar,
     Backdrop,
     Button,
-    FormControl,
-    FormGroup,
     makeStyles,
     Modal,
     TextField,
     Fade,
-    Box, Typography
+    Box, FormGroup
 } from "@material-ui/core";
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import login from "./Icons/login.png";
+import register from "./Icons/register.png";
+import success from "./Icons/success.png";
 
 import {USER_LOGIN} from "../../store/Actions/Auth/Login";
 
@@ -31,17 +30,17 @@ const useStyles = makeStyles(theme=> ({
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        outline: "none"
+        gap: theme.spacing(2),
+        outline: "none",
+        paddingLeft: "3rem",
+        paddingRight: "3rem",
+        borderRadius: "5px"
     },
     formIcon: {
-        marginTop: theme.spacing(7),
+        marginTop: theme.spacing(5),
         marginBottom: theme.spacing(5),
-        width: theme.spacing(10),
-        height: theme.spacing(10)
-    },
-    formItem: {
-        marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(1)
+        width: theme.spacing(12),
+        height: theme.spacing(12)
     },
     loginSuccess: {
         width: "20rem",
@@ -54,15 +53,10 @@ const useStyles = makeStyles(theme=> ({
         outline: "none"
     },
     loginSuccessIcon: {
-        width: theme.spacing(8),
-        height: theme.spacing(8),
+        width: theme.spacing(12),
+        height: theme.spacing(12),
         marginBottom: theme.spacing(5),
-        background: theme.palette.success.main
-    },
-    loginSuccessText: {
-        fontFamily: "Playfair Display, serif",
-        color: "green",
-        marginBottom: theme.spacing(5)
+        borderRadius: "5px"
     }
 }))
 
@@ -71,43 +65,66 @@ const Auth = (props) => {
     const classes = useStyles()
     const {isAuthenticated} = props.auth
     const [auth, setAuth] = useState({
-        username: "",
-        password: ""
+        username: '',
+        password: ''
+    })
+    const [error, setError] = useState({
+        'username': false,
+        'password': false
     })
     const onChangeHandler = (e) => {
         setAuth({
             ...auth,
             [e.target.name]: e.target.value
         })
+        setError({
+            ...error,
+            [e.target.name]: false
+        })
     }
 
     const onSubmitHandler = () => {
-        props.USER_LOGIN(auth.username, auth.password)
+        if(auth.username.length && auth.password.length) {
+            props.USER_LOGIN(auth.username, auth.password)
+        }
+        else if(!auth.username.length && !auth.password.length){
+            setError({
+                ...error,
+                username: true,
+                password: true
+            })
+        }
+        else if(!auth.username.length){
+            setError({
+                ...error,
+                username: true
+            })
+        } else if(!auth.password.length){
+            setError({
+                ...error,
+                password: true
+            })
+        }
     }
 
     const loginForm = (
         <Fade in={props.open}>
                 <FormGroup className={classes.formGroup} aria-autocomplete={"none"}>
-                    <FormControl>
-                        <Avatar className={classes.formIcon}><AccountCircleIcon style={{fontSize: 40}}/></Avatar>
-                    </FormControl>
-                    <FormControl className={classes.formItem}>
-                        <TextField name="username" variant="outlined" label="username" onChange={(event)=>onChangeHandler(event)}/>
-                    </FormControl>
-                    <FormControl className={classes.formItem}>
-                        <TextField name="password" variant="outlined" label="password" onChange={(event)=>onChangeHandler(event)}/>
-                    </FormControl>
-                    <FormControl>
-                        <Button variant="contained" color="primary" onClick={onSubmitHandler} className={classes.formItem}>login</Button>
-                    </FormControl>
+                    <Avatar src={"/static/bundles/"+login} variant={"square"} className={classes.formIcon}/>
+                    <TextField name="username" variant="outlined" label="username" fullWidth error={error.username}
+                               helperText={error.username ? 'Enter username': null}
+                               onChange={(event)=>onChangeHandler(event)}/>
+                    <TextField name="password" variant="outlined" label="password" fullWidth  error={error.password}
+                               helperText={error.password ? 'Enter password': null}
+                               onChange={(event)=>onChangeHandler(event)}/>
+                    <Button variant="contained" color="primary" onClick={onSubmitHandler} fullWidth>login</Button>
                 </FormGroup>
             </Fade>
     )
 
     const loginSuccess = (
         <Box className={classes.loginSuccess}>
-            <Avatar className={classes.loginSuccessIcon}><CheckCircleIcon fontSize="large"/></Avatar>
-            <Typography variant="h4" className={classes.loginSuccessText}>Success</Typography>
+            <Avatar className={classes.loginSuccessIcon} src={"/static/bundles/"+success} variant={"square"}/>
             <Button onClick={()=>props.onClose()} variant="contained" color="primary" size="large">OK</Button>
         </Box>
     )
